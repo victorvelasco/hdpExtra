1e5c16e27127398868f0453ad7bb35cebe7cc932
Victor Velasco Pardo <victorvelasco@outlook.com>
2023-05-10 16:07:55 +0100

    vignette

--- R/hdpExtra_diagnostic_plots.R
+++ R/hdpExtra_diagnostic_plots.R
@@ -20,3 +20,21 @@ hdpExtra_diagnostic_plots <- function(hdpExtraChainMulti, param_names) {
     # ggmcmc::ggs_compare_partial(ggs_object)
   )
 }
+
+#' Autocorrelation plots for the concentration parameters
+#'
+#' @param hdpExtraChainMulti An object of class HdpExtraChainMulti
+#' @param param_names Name of the concentration parameters, to display in
+#' the diagnostic plots
+#'
+#' @export
+hdpExtra_acf <- function(hdpExtraChainMulti, param_names) {
+  list_cp_values <- lapply(
+    1:length(hdpExtraChainMulti@cp_values),
+    function(i) coda::as.mcmc(hdpExtraChainMulti@cp_values[[i]][-(1:chlist@burnin[i]), ])
+  )
+  list_cp_values <- coda::as.mcmc.list(list_cp_values)
+  coda::varnames(list_cp_values) <- param_names
+  list_cp_values <- ggmcmc::ggs(list_cp_values, keep_original_order = TRUE)
+  list(ggmcmc::ggs_autocorrelation(list_cp_values, nLags = 200))
+}
